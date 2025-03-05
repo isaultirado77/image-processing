772a6ff73c8b871b4bc6e694b1ee99313f41e325
Isaul <tiradolisaul@gmail.com>
2025-03-04 19:38:10 -0700

    initial skeleton of ImageLoader class implemented

--- core/image_loader.py
+++ core/image_loader.py
@@ -5,4 +5,29 @@ import urllib.request
 from pathlib import Path
 
 class ImageLoader: 
+    """
+    Clase para cargar imagenes desde archivos locales o URLs.
+    """
+    def __init__(self, source: str):
+        """..."""
+        self._soruce = source
+        self._image = None
+    
+    def load(self):
+        """...""" 
+        pass
+
+    def _is_url(self):
+        """...""" 
+        pass
+
+    def _load_from_url(self):
+        """...""" 
+        pass
+
+    def _load_from_file(self):
+        """...""" 
+        pass
+
+if __name__ == "__main__": 
     pass
