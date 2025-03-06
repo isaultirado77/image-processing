308718391ae6d2ddfd486fbee7522acf1c72514e
Isaul <tiradolisaul@gmail.com>
2025-03-05 18:30:39 -0700

    (polygon, text, show, save) methods in Drawing class:  implemented

--- core/drawing.py
+++ core/drawing.py
@@ -2,20 +2,20 @@ import cv2
 import numpy as np
 
 class Drawing: 
-    def __init__(self, width: int = 256, height: int = 256, background: tuple = (0, 0, 0)):
+    def __init__(self, width=255, height=255, background=(0, 0, 0)):
         """..."""
         self._canvas = np.zeros((height, width, 3), dtype="unit8")
         self._canvas[:] = background
 
-    def line(self, start: tuple, end: tuple, color: tuple = (256, 256, 256), thickness: int = 1):
+    def line(self, start: tuple, end: tuple, color=(255, 255, 255), thickness=1):
         """..."""
         cv2.line(self._canvas, start, end, color, thickness)
 
-    def rectangle(self, top_left: tuple, bottom_right: tuple, color: tuple = (256, 256, 256), thickness: int = 1): 
+    def rectangle(self, top_left: tuple, bottom_right: tuple, color=(255, 255, 255), thickness=1): 
         """..."""
         cv2.rectangle(self.canvas, top_left, bottom_right, color, thickness)
 
-    def circle(self, center: tuple, radius: int, color: tuple = (256, 256, 256), thickness: int = 1):
+    def circle(self, center: tuple, radius: int, color=(255, 255, 255), thickness=1):
         """...""" 
         cv2.circle(center, radius, color, thickness)
 
@@ -27,18 +27,21 @@ class Drawing:
         """..."""
         pass
 
-    def polygon(self): 
+    def polygon(self, points: tuple, color=(255, 255, 255), thickness=1): 
         """..."""
-        pass
+        pts = np.array(points, np.int32).reshape((-1, 1, 2))
+        cv2.polylines(self._canvas, [pts], isClosed=True, color=color, thickness=thickness)
 
-    def text(self): 
+    def text(self, text: str, position: tuple, font_scale=1, color=(255, 255, 255), thickness=2): 
         """..."""
-        pass
+        cv2.putText(self._canvas, text, position, cv2.FONT_HERSHEY_PLAIN, font_scale, color, thickness)
 
-    def show(self): 
+    def show(self, name="Drawing"): 
         """..."""
-        pass
+        cv2.imshow(mat=self._canvas, winname=name)
+        cv2.imshow(0)
+        cv2.destroyAllWindows()
 
-    def save(self): 
+    def save(self, path="drawing.png"): 
         """..."""
-        pass
+        cv2.imwrite(path, self._canvas)
