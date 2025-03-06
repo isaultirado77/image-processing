a5bde1903c690ded3aac0f9b99648f3cb67304b8
Isaul <tiradolisaul@gmail.com>
2025-03-05 18:18:43 -0700

    (line, rectangle, circle) methods in Drawing class:  implemented

--- core/drawing.py
+++ core/drawing.py
@@ -2,21 +2,22 @@ import cv2
 import numpy as np
 
 class Drawing: 
-    def __init__(self):
+    def __init__(self, width: int = 256, height: int = 256, background: tuple = (0, 0, 0)):
         """..."""
-        pass
+        self._canvas = np.zeros((height, width, 3), dtype="unit8")
+        self._canvas[:] = background
 
-    def line(self):
+    def line(self, start: tuple, end: tuple, color: tuple = (256, 256, 256), thickness: int = 1):
         """..."""
-        pass
+        cv2.line(self._canvas, start, end, color, thickness)
 
-    def rectangle(self): 
+    def rectangle(self, top_left: tuple, bottom_right: tuple, color: tuple = (256, 256, 256), thickness: int = 1): 
         """..."""
-        pass
+        cv2.rectangle(self.canvas, top_left, bottom_right, color, thickness)
 
-    def circle(self):
+    def circle(self, center: tuple, radius: int, color: tuple = (256, 256, 256), thickness: int = 1):
         """...""" 
-        pass
+        cv2.circle(center, radius, color, thickness)
 
     def random_circle(self): 
         """..."""
