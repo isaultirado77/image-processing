3b70aef367f1320fdc016bef956aae5c92fdf222
Isaul <tiradolisaul@gmail.com>
2025-03-07 13:24:18 -0700

    image rotation chapter completed

---
+++ core/image_transform.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+class ImageTransform: 
+    def __init__(self):
+        pass
+
+    def translation(self): 
+        pass
+
+    def rotation(self): 
+        pass
+    
