6cd2e69624c5a1b09b65a19128b4ab242ce7df91
Isaul <tiradolisaul@gmail.com>
2025-03-12 19:11:46 -0700

    re-init loader class on image_loader module

--- core/image_loader.py
+++ core/image_loader.py
@@ -1,66 +1,8 @@
-import argparse
 import cv2
-import numpy as np
-import urllib.request
 from pathlib import Path
 
-class Loader: 
-    """
-    Clase para cargar imagenes desde archivos locales o URLs.
-    """
-    def __init__(self, source: str):
-        """..."""
-        self._soruce = source
-        self._image = None
-    
-    def load(self):
-        """
-        Carga la imagen desde la fuente específicada. 
-        : return: Imagen cargada en formato OpenCV (numpy array)
-        """ 
-        if self._is_url(): 
-            print("\n---URL---\n")
-            self._image = self._load_from_url()
-        else: 
-            print("\n---FILE---\n")
-            self._image = self._load_from_file()
-        
-        if self._image is None: 
-            raise ValueError("The image could not be loaded. Check the path or URL. ")
+import cv2
+from pathlib import Path
 
-    def _is_url(self):
-        """Verifica si la fuente es una URL.""" 
-        # return self._soruce.startswith("http://") or self._soruce.startswith("https://")
-        return False
-    
-    def _load_from_url(self):
-        """Carga una imagen desde una URL."""
-        try: 
-            response = urllib.request.urlopen(self._soruce)
-            image_array = np.asarray(bytearray(response.read(), dtype=np.uint8))
-            return cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-        except Exception as e: 
-            print(f"Error while loading image from URL: {e}")
-            return 
-        
-    def _load_from_file(self):
-        """Carga una imagen desde un archivo local.""" 
-        if not Path(self._soruce).is_file(): 
-            print("Error: The source file doesn't exists.")
-            return
-        return cv2.imread(self._soruce)
-    
-    def get_image(self): 
-        return self._image
-
-if __name__ == "__main__": 
-    parser = argparse.ArgumentParser(description="Carga una imagen desde un archivo o URL.")
-    parser.add_argument("-i", "--image", required=True, help="Ruta del archivo o URL de la imagen.")
-    args = vars(parser.parse_args())
-
-    loader = Loader(args["image"])
-    image = loader.load()
-
-    cv2.imshow("Image", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+class Loader:
+    pass
--- learning/crop.py
+++ learning/crop.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 from core.image_loader import Loader
 
-image_path = "C:\Users\isaul\Documents\image-processing-main\images\lenna.tiff"
-loader = Loader(source=image_path).load()
+image_path = "lenna.tiff"
+loader = Loader(image_path)
+loader.load()
 
 image = loader.get_image()
 cv2.imshow("Original", image)
+cv2.waitKey(0)
