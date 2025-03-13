e6413c917f37bbc1eacc72ed52fc25ebbe9d3b62
Isaul <tiradolisaul@gmail.com>
2025-03-12 19:35:19 -0700

    image_loader module corrected: file & url source working

--- core/image_loader.py
+++ core/image_loader.py
@@ -1,8 +1,45 @@
-import cv2
-from pathlib import Path
-
+import urllib.request
+import numpy as np
 import cv2
 from pathlib import Path
 
 class Loader:
-    pass
+    IMAGES_DIR = Path(__file__).resolve().parent.parent / "images"
+
+    def __init__(self, source: str):
+        self._source = source
+        self._image = None
+
+    def load(self):
+        if self._is_url():
+            print(f"\n--- Loading from URL: {self._source} ---\n")
+            self._image = self._load_from_url()
+        else:
+            print(f"\n--- Loading from File: {self._source} ---\n")
+            self._image = self._load_from_file()
+
+        if self._image is None:
+            raise ValueError("The image could not be loaded. Check the path or URL.")
+
+    def _is_url(self):
+        return self._source.startswith("http://") or self._source.startswith("https://")
+
+    def _load_from_url(self):
+        try:
+            response = urllib.request.urlopen(self._source)
+            image_array = np.asarray(bytearray(response.read()), dtype=np.uint8)
+            return cv2.imdecode(image_array, cv2.IMREAD_COLOR)
+        except Exception as e:
+            print(f"Error while loading image from URL: {e}")
+            return None
+
+    def _load_from_file(self):
+        file_path = self.IMAGES_DIR / self._source
+        if not file_path.is_file():
+            print(f"Error: The source file doesn't exist. Path: {file_path}")
+            return None
+        return cv2.imread(str(file_path))
+
+    def get_image(self):
+        return self._image
+
--- learning/crop.py
+++ learning/crop.py
@@ -2,8 +2,11 @@ import cv2
 import numpy as np
 from core.image_loader import Loader
 
-image_path = "lenna.tiff"
-loader = Loader(image_path)
+# source = "https://pesdb.net/pes2020/images/players/7511.png"
+# source = r"C:\Users\isaul\Downloads\Jaguar - Ian Ford.jpeg"
+source = "lenna.tiff"
+
+loader = Loader(source)
 loader.load()
 
 image = loader.get_image()
