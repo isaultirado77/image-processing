18c16d276f8463e366b6e9804f0f58c5a72b5714
Isaul <tiradolisaul@gmail.com>
2025-03-31 18:57:23 -0700

    image filter module ready for dev

---
+++ core/image_filter.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+
+### 1. Detección de Contornos y Formas ###
+def find_contours():
+    """
+    """
+    pass
+
+def approximate_contour():
+    """
+    """
+    pass
+
+def detect_shapes(): 
+    """
+    """
+    pass
+
+
+### 2. Segmentación ###
+def threshold():
+    """
+    """
+    pass
+
+def adaptive_threshold():
+    """
+    """
+    pass
+
+def watershed_segmentation(): 
+    pass 
+
+### 3. Análisis de Características ###
+def compute_moments(): 
+    """
+    """
+    pass
+
+def compute_shape_descriptors():
+    """
+    """
+    pass
+
+### 4. Detección de Bordes y Esquinas ###
+def detect_edges(): 
+    """
+    """
+    pass
+
+def detect_corners(): 
+    """
+    """
+    pass
+
+### 5. Visualización ###
+def draw_contours(): 
+    """
+    """
+    pass
+
+def draw_corners(): 
+    """
+    """
+    pass
