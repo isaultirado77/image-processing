1a8fa778707c63ee833b6ad0784f666010ba003a
Isaul <tiradolisaul@gmail.com>
2025-04-02 18:50:47 -0700

    init frecuency filters module

---
+++ core/frecuency_filters.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+
+def compute_fft():
+    pass
+
+def apply_fft_filter():
+    pass
+
+def create_ideal_filter():
+    pass
+
+def create_butterworth_filter():
+    pass
+
+def create_gaussian_filter(): 
+    pass
